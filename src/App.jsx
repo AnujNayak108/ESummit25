a9bff4e616355f9cd25b6acf31bc2d73153640ba
Naveen Kumar Modi <154827648+Navinco@users.noreply.github.com>
2024-12-30 14:21:27 +0000

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import SponsorshipSection from './components/SponsorshipSection';
 
 function App() {
 
   return (
     <>
-      
+      <SponsorshipSection />;
     </>
   )
 }
