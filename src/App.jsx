73a8a254c3519c8ca4e58abaa15b87103fbaf950
sarthak <sarthak613singh@gmail.com>
2025-01-01 11:36:18 +0530

    adding about_edc section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React from "react"
-import PastSpeakersmain from "./components/past speakers/Pastspeakermain"
-import FeaturedSpeakers from "./components/FeaturedSpeaker/FS"
-import SponsorshipSection from "./components/SponsorshipSection"
+import React from "react";
+import PastSpeakersmain from "./components/past speakers/Pastspeakermain";
+import FeaturedSpeakers from "./components/FeaturedSpeaker/FS";
+import SponsorshipSection from "./components/SponsorshipSection";
+import AboutUs from "./components/AboutUs/AboutUs";
 
 function App() {
-
   return (
     <>
-    <FeaturedSpeakers/>
-    <PastSpeakersmain/>
-    <SponsorshipSection/>
+      <AboutUs />
+      <FeaturedSpeakers />
+      <PastSpeakersmain />
+      <SponsorshipSection />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(circle at left, var(--tw-gradient-stops))',
+        "gradient-radial":
+          "radial-gradient(circle at left, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+        jakarta: ["Plus Jakarta Sans", "sans-serif"],
+      },
+      colors: {
+        primaryGreen: "#00B25C",
       },
     },
   },
